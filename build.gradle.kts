879043416793bf00267bf458603723430c6f76d8
likewu <likewu@126.com>
2022-10-24 21:51:50 +0800

    aa

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("com.android.tools.build:gradle:7.0.4")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.8.2.1")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
